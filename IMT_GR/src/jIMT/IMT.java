43c65bca90dce4fbc51faeaddbf4508f2b63d378
Arhsota <arhsota@gmail.com>
2018-02-22 22:53:01 +0300

    concat digits and text in output

--- IMT_GR/src/jIMT/IMT.java
+++ IMT_GR/src/jIMT/IMT.java
@@ -1,13 +1,14 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. m
  */
 package jIMT;
 
 /**
  *
  * @author arhso
+ * @ver 1.0
  */
 public class IMT extends javax.swing.JFrame {
     private double weight = 0.0;
